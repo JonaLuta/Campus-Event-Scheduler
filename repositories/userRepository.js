3e1e735997fa3f7f1e982f392499cc428473c008
JonaLuta <jonaluta984@gmail.com>
2025-12-04 19:14:53 +0100

    Update userRepository.js

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,4 +1,25 @@
-export class UserRepository {
-  findById(id) { return null; }
-  save(user) { return user; }
+class UserRepository {
+  constructor() {
+    this.users = [];
+  }
+
+  findById(id) {
+    return this.users.find((u) => u.id === id) || null;
+  }
+
+  save(user) {
+    const index = this.users.findIndex((u) => u.id === user.id);
+    if (index >= 0) {
+      this.users[index] = user;
+    } else {
+      this.users.push(user);
+    }
+    return user;
+  }
+
+  findAll() {
+    return [...this.users];
+  }
 }
+
+module.exports = UserRepository;
