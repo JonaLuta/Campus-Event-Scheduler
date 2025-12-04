f094462199d492bbcf47eaf219c70c876a5270e6
JonaLuta <jonaluta984@gmail.com>
2025-12-04 19:14:04 +0100

    Update eventRepository.js

--- repositories/eventRepository.js
+++ repositories/eventRepository.js
@@ -1,5 +1,25 @@
-export class EventRepository {
-  findById(id) { return null; }
-  findAll() { return []; }
-  save(event) { return event; }
+class EventRepository {
+  constructor() {
+    this.events = [];
+  }
+
+  findById(id) {
+    return this.events.find((e) => e.id === id) || null;
+  }
+
+  save(event) {
+    const index = this.events.findIndex((e) => e.id === event.id);
+    if (index >= 0) {
+      this.events[index] = event;
+    } else {
+      this.events.push(event);
+    }
+    return event;
+  }
+
+  findAll() {
+    return [...this.events];
+  }
 }
+
+module.exports = EventRepository;
